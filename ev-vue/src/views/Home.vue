7a623af549669a40b2ec1f30ecb67a99eb9f04b5
annasm07 <anna.sala.mercade@gmail.com>
2021-10-28 11:04:03 +0200

    Structure App, Header and Home components
    To have the nav in the Header component instead of App

--- ev-vue/src/views/Home.vue
+++ ev-vue/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <Header msg="Hello World"/>
+   <p>THIS IS HOME PAGE</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from '../components/Header.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Header,
+
   },
 });
 </script>
